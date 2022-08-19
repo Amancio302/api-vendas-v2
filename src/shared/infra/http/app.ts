cfd66ee1562d61dcb5fd01e893b1ed0d8616841d
Amancio302 <matheusamancio.dev@gmail.com>
2022-08-19 09:32:24 -0300

    removed rate limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -14,9 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(rateLimiter);
+// app.use(rateLimiter);
 
-app.use('/files', express.static(uploadConfig.directory));
+// app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
